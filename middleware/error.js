b4689ec427be803835350d325f84683a27579d8f
vshyam121 <v.shyam121@gmail.com>
2020-08-13 17:37:30 -0700

    Taking care of producing meaningful error messages for common errors

---
+++ middleware/error.js
@@ -0,0 +1,34 @@
+const ErrorResponse = require('../utils/ErrorResponse');
+
+//@desc Sends back more human readable error messages for common cases
+const errorHandler = (err, req, res, next) => {
+  //Log to console for dev
+  console.log(err.stack.red);
+  console.log(err.message);
+
+  let error = { ...err };
+
+  error.message = err.message;
+
+  if (err.name === 'CastError') {
+    const message = `Resource not found with id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  //Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
+  res
+    .status(error.statusCode || 500)
+    .json({ success: false, message: error.message || 'Server Error' });
+};
+
+module.exports = errorHandler;
