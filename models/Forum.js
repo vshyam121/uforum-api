1fd61b8f750c6d731545797c8f4fe40dfc89d92e
vshyam121 <v.shyam121@gmail.com>
2020-08-18 23:33:25 -0700

    Set slug to be unique and validate uniqueness

--- models/Forum.js
+++ models/Forum.js
@@ -1,21 +1,25 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const ForumSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, 'Forum name is required'],
+    unique: true,
   },
-  slug: String,
+  slug: {
+    type: String,
+    unique: true,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
   },
 });
 
-ForumSchema.pre('save', function (next) {
-  this.slug = slugify(this.name, { lower: true });
-  next();
+ForumSchema.plugin(uniqueValidator, {
+  message: 'Error, expected {PATH} to be unique.',
 });
 
 module.exports = mongoose.model('Forum', ForumSchema);
